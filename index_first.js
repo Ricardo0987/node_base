817edfa7f8d7d717ac3d19eb5b734c0b092ebb25
MasterBoy <ricardorlo@hotmail.es>
2023-05-14 13:29:44 -0500

    ...

---
+++ index_first.js
@@ -0,0 +1,17 @@
+console.log("test");
+require("dotenv").config();
+
+console.log(process.env);
+
+const figlet = require("figlet");
+
+const myVar = process.env.VAR_REPO_1;
+console.log(
+  figlet.textSync(myVar, {
+    //   font: "Ghost",
+    horizontalLayout: "default",
+    verticalLayout: "default",
+    width: 80,
+    whitespaceBreak: true,
+  })
+);
