dda93bd5e7161be63ed9bb43a0452f53bf936fda
MasterBoy <ricardorlo@hotmail.es>
2023-05-14 20:26:25 -0500

    ..

--- index_first.js
+++ index_first.js
@@ -19,7 +19,7 @@ console.log(
 const newObj = { a: 22, b: "nuevo objeto definido in exec", c: true };
 const fs = require("fs");
 
-if (process.env.ENV_PATH_TOCHANGE.length > 0) {
+if (process.env.ENV_PATH_TOCHANGE) {
   fs.appendFile(
     process.env.ENV_PATH_TOCHANGE,
     `\r\nDATA_RECEIVED='${JSON.stringify(newObj)}'`,
