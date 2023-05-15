ad43c12a2a18423c651f783d83446065b465791f
MasterBoy <ricardorlo@hotmail.es>
2023-05-14 20:30:46 -0500

    ..

--- index_first.js
+++ index_first.js
@@ -1,20 +1,4 @@
-console.log("test");
 require("dotenv").config();
-
-console.log(process.env);
-
-const figlet = require("figlet");
-
-const myVar = process.env.VAR_REPO_1;
-console.log(
-  figlet.textSync(myVar, {
-    //   font: "Ghost",
-    horizontalLayout: "default",
-    verticalLayout: "default",
-    width: 80,
-    whitespaceBreak: true,
-  })
-);
 
 const newObj = { a: 22, b: "nuevo objeto definido in exec", c: true };
 const fs = require("fs");
@@ -29,7 +13,3 @@ if (process.env.ENV_PATH_TOCHANGE) {
     }
   );
 }
-
-/* 
-ENV_PATH_TOCHANGE="../node_base2/.env" node index_first.js
-*/
